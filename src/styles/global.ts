c6e7daf4d4b5d3d9e05520ceb98236a19bdb8800
Dutrak <fernandodutra100701@gmail.com>
2024-06-15 16:30:53 -0300

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  input {
+    border-radius: 6px;
+    border: 1px solid ${(props) => props.theme['base-border']};
+    background-color: ${(props) => props.theme['base-input']};
+  }
+
+  input:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.blue};
+  }
+
+  body {
+    background-color: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']}
+  }
+
+
+`
